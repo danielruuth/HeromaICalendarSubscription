908525388841dee94ba4ed5c00319056a14daa5d
danielruuth <daniel.ruuth@gmail.com>
2023-03-12 12:00:12 +0100

    Update server.js

--- node/server.js
+++ node/server.js
@@ -2,6 +2,7 @@ const https = require("https");
 const fs = request("fs");
 const moment = require("moment");
 const auth = require("auth")
+const schedule = require("calendar")
 moment().format(); 
 
 // Import the express module
@@ -23,51 +24,14 @@ app.get('/', (req,res)=>{
 
 app.get('/ical/:user/:password/:months', (req, res) =>{
   
-  const {token, verificationToken} = await auth(req.params.user, req.params.password);
-  
-  const now = moment();
-  const end = moment().add(months, 'months');
-  
-  const postData = JSON.stringify({
-    StartString: now.format('YYYY-MM-DD'), //YYYY-mm-dd
-    StopString: end.format('YYYY-MM-DD'), //YYYY-mm-dd
-    ShowWork: true,
-    ShowAbs: true,
-    ShowApp: true,
-    ShowTCall: true,
-    ShowTaskWeekComments: true,
-    ShowTaskWeekStaffComments: true,
-    ShowWeekStaff: true,
-    __RequestVerificationToken: verificationToken //This should be VerificationToken from cookie
-  });
-  
-  const options = {
-    hostname: 'https://heroma.vgregion.se',
-    path: '/Webbklient/api/APCalendarApi/getCalendarFile',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Cookie': token, //This should be the cookie token from loggin
-    },
-  };
-  const request = http.request(options, (response) => {
-    response.setEncoding('utf8');
-
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      console.log(data); //This should be served as an ics file
-    });
-  });
-
-  // Log errors if any occur
-  request.on('error', (error) => {
-    console.error(error);
-  });
-
-  // End the request
-  request.write(postData)
-  request.end();
+  try{
+    const {token, verificationToken} = await auth(req.params.user, req.params.password);
+    const calendarData = schedule(verificationToken, token, req.params.months)
+    
+    res.setHeader('Content-Type', 'text/calendar');
+    res.send(calendarData);
+   }catch(erro){
+    console.error(erro)
+     res.status(500).res('Error');
+   }
 })
